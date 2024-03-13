60516743e562be44ed12bf173dfb1c8d21fd9847
Robby Sarvis <rjs.app.dev@gmail.com>
2024-03-13 15:27:19 -0500

    updoot

--- TestThing/ContentView.swift
+++ TestThing/ContentView.swift
@@ -10,12 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
-        }
-        .padding()
+                .padding()
+
+            Spacer()
+        }.background(Color.purple)
     }
 }
 
