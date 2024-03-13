785656d7ba20824b045fafc455c0814c8d6389ba
Robby Sarvis <rjs.app.dev@gmail.com>
2024-03-13 15:25:42 -0500

    Initial Commit

---
+++ TestThing/TestThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestThingApp.swift
+//  TestThing
+//
+//  Created by Robert J. Sarvis Jr on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TestThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
